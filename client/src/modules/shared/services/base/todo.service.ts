818f6e7a6962efdf10acd519da083aedfb9ec9eb
Steffen Jahr <steffen.jahr@thinktecture.com>
2017-12-15 15:26:01 +0100

    Initial commit

---
+++ client/src/modules/shared/services/base/todo.service.ts
@@ -0,0 +1,50 @@
+import {DatabaseService} from './database.service';
+import {ITodoItem} from '../../models/contracts/todoItem.interface';
+import Dexie from 'dexie';
+
+export class TodoService {
+    protected table: Dexie.Table<ITodoItem, number>;
+
+    constructor(private _databaseService: DatabaseService) {
+        this.table = this._databaseService.table('todos');
+    }
+
+    public getAll(includeDeleted: boolean): Promise<Array<ITodoItem>> {
+        if (includeDeleted) {
+            // TODO
+        }
+        // TODO
+    }
+
+    public add(item: ITodoItem): Promise<number> {
+        // TODO
+    }
+
+    public async update(item: ITodoItem): Promise<number> {
+        item.changed = true;
+        // TODO
+    }
+
+    public async delete(item: ITodoItem): Promise<number> {
+        if (!item.syncId) {
+            // TODO: delete from table
+        } else {
+            item.deleted = true;
+            // TODO: mark as deleted and update record
+        }
+    }
+
+    public async overwrite(list: Array<ITodoItem>): Promise<Array<ITodoItem>> {
+        // Delete IDs to prevent duplicate key
+        list.forEach(item => delete item.id);
+
+        await this.table.clear();
+        await this.table.bulkAdd(list);
+        return this.getAll(false);
+    }
+
+    public clear(): Promise<void> {
+        return this.table.clear();
+    }
+}
+
